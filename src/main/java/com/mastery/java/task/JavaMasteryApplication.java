cab05d7f05d0fc4cd1f86b91b75c60fe23379694
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-05-14 19:31:26 +0300

    init: initial commit

---
+++ src/main/java/com/mastery/java/task/JavaMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.mastery.java.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMasteryApplication.class, args);
+	}
+
+}
