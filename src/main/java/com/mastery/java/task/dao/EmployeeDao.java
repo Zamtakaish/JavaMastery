a039925824255834cec36490f614a27ec14fbca0
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-05-15 22:22:47 +0300

    fix: revork DAO class structure

--- src/main/java/com/mastery/java/task/dao/EmployeeDao.java
+++ src/main/java/com/mastery/java/task/dao/EmployeeDao.java
@@ -1,4 +1,39 @@
 package com.mastery.java.task.dao;
 
+import com.mastery.java.task.dto.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class EmployeeDao {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+
+    public Employee getEmployee(int id) {
+        Optional<Employee> employee = repository.findById(id);
+        if (employee.isEmpty()){
+            throw new RuntimeException("Employee is not found with ID: " + id);
+        }
+        return employee.get();
+    }
+
+    public void createEmployee(Employee employee){
+        repository.save(employee);
+    }
+
+    public void updateEmployee(int id, Employee employee){
+        repository.save(employee);
+    }
+
+    public void deleteEmployee(int id){
+        repository.deleteById(id);
+    }
 }
