6f7d7ee8affeb768f42687ccc280376a48ec1891
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-05-15 22:23:23 +0300

    feat: implement employee controller

--- src/main/java/com/mastery/java/task/rest/EmployeeController.java
+++ src/main/java/com/mastery/java/task/rest/EmployeeController.java
@@ -1,4 +1,42 @@
 package com.mastery.java.task.rest;
 
+import com.mastery.java.task.dto.Employee;
+import com.mastery.java.task.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public void getEmployee(@PathVariable int id) {
+        employeeService.getEmployee(id);
+    }
+
+    @PostMapping("/employees")
+    public void createEmployee(@RequestBody Employee employee){
+        employeeService.createEmployee(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public void updateEmployee(@PathVariable int id, @RequestBody Employee employee){
+        employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable int id){
+        employeeService.deleteEmployee(id);
+    }
+
 }
