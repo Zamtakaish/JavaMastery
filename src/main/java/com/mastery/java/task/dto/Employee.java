70c4999c423f9f57151815e7631c1500eaa3a073
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-05-15 20:13:13 +0300

    feat: implement Employee class structure

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -1,11 +1,63 @@
 package com.mastery.java.task.dto;
 
+import javax.persistence.*;
+
+@Entity
 public class Employee {
-    private Short employeeId;
+
+    @Id
+    @GeneratedValue
+    private Integer employeeId;
+
     private String firstName;
     private String lastName;
     private Short departmentId;
     private String jobTitle;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
     private String dateOfBirth;
+
+    public Employee() {
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Short getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", departmentId=" + departmentId +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", gender=" + gender +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
 }
