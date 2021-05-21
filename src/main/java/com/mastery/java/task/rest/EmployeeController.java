5d5ea7257288bb96d0710c5ca8ce43e06323f7ed
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-05-21 23:33:17 +0300

    refactor: annotations rework

--- src/main/java/com/mastery/java/task/rest/EmployeeController.java
+++ src/main/java/com/mastery/java/task/rest/EmployeeController.java
@@ -3,38 +3,41 @@ package com.mastery.java.task.rest;
 import com.mastery.java.task.dto.Employee;
 import com.mastery.java.task.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/employees")
+    @GetMapping("")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
-    @GetMapping("/employees/{id}")
+    @GetMapping("{id}")
     public Employee getEmployee(@PathVariable int id) {
         return employeeService.getEmployee(id);
     }
 
-    @PostMapping("/employees")
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createEmployee(@RequestBody Employee employee){
         employeeService.createEmployee(employee);
     }
 
-    @PutMapping("/employees/{id}")
+    @PutMapping("{id}")
     public void updateEmployee(@PathVariable int id, @RequestBody Employee employee){
         employeeService.updateEmployee(id, employee);
     }
 
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping("{id}")
     public void deleteEmployee(@PathVariable int id){
         employeeService.deleteEmployee(id);
     }
